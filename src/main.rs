991162ada12a00ef6d873dd345c062b361626b0b
Ben Hamlen <hamlen.ben@gmail.com>
2022-12-27 10:52:59 -0600

    got caching working, ready for first test on dads computer

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ mod args;
 use clap::Parser;
 use args::ReverseImageSearchArgs;
 use image::ImageError;
+use rayon::prelude::IntoParallelRefIterator;
 use std::fs::File;
 use std::fs;
 use std::io::{self, BufRead};
+use std::ops::Index;
 use std::path::Path;
 use image_hasher::{HasherConfig, ImageHash};
 use image::imageops::FilterType;
@@ -15,14 +17,129 @@ use kdam::tqdm;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
+const CONFIG_PATH_DEFAULT: &str = "config.json";
+const CACHE_PATH: &str = "cache.json";
+const IMAGE_RESIZE_SIZE: [u32; 2] = [100,100];
+
+#[derive(Debug, Serialize, Deserialize)]
+// #[derive(Debug)]
+struct CachedImgInfo{
+    /// the ImageHash object doesn't work with serde so will be saving the base64 representation then
+    /// using the ImageHash::from_base64() method to create a new instance from the base64 representation
+    hash_base64: String,
+    path: String,
+    dist: u32
+}
+
+impl CachedImgInfo {
+    fn from_noncached(info: ImgInfo) -> CachedImgInfo {
+        CachedImgInfo { hash_base64: info.hash.to_base64(),
+                        path: info.path,
+                        dist: info.dist}
+    }
+
+    fn to_noncached(&self) -> ImgInfo {
+        ImgInfo {   hash: ImageHash::from_base64(&self.hash_base64).unwrap(),
+                    path: self.path.clone(),
+                    dist: self.dist }
+    }
+}
+
+struct ImgInfo {
+    hash: ImageHash,
+    path: String,
+    dist: u32
+}
+
+impl ImgInfo {
+    fn from_cached(info: CachedImgInfo) -> ImgInfo {
+        ImgInfo {   hash: ImageHash::from_base64(&info.hash_base64).unwrap(),
+                    path: info.path,
+                    dist: info.dist}
+    }
+
+    fn to_cached(&self) -> CachedImgInfo {
+        CachedImgInfo { hash_base64: self.hash.to_base64(),
+                        path: self.path.clone(),
+                        dist: self.dist }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Cache {
+    info: Vec<CachedImgInfo>
+}
+
+impl Cache {
+
+    fn merge(&mut self, imginfos: Vec<ImgInfo>) {
+        for entry in imginfos {
+            self.info.push(CachedImgInfo { hash_base64: entry.hash.to_base64(), path: entry.path.clone(), dist: entry.dist })
+        }
+    }
+
+    fn blank() -> Cache {
+        Cache { info: Vec::new() }
+    }
+
+    fn new() -> Cache {
+
+        /* if existing cache, load and return that */
+        if Path::new(CACHE_PATH).exists() {
+            println!("loading cache from file");
+            return Cache::from_file(CACHE_PATH);
+        }
+
+        /* if no existing cache, return a new blank one */
+        else {
+            println!("no cache found, making new one");
+            return Cache::blank()
+        }
+    }
+
+    fn get_all_paths(&self) -> Vec<String> {
+        let mut paths: Vec<String> = Vec::new();
+
+        for entry in self.info.iter() {
+            paths.push(entry.path.clone());
+        }
+
+        paths
+    }
+
+    fn from_vec(data: Vec<CachedImgInfo>) -> Cache{
+        Cache { info: data }
+    }
+
+    fn from_file(path: &str) -> Cache {
+        let cache_string = fs::read_to_string(path)
+            .expect("Unable to read file");
+        serde_json::from_str::<Cache>(cache_string.as_str())
+            .expect("Unable to deserialize cache json file")
+    }
+
+    fn save(&self) {
+        let cache_string = serde_json::to_string(&self)
+            .expect("unable to serialize cache into string");
+        fs::write(CACHE_PATH, cache_string)
+            .expect("Unable to write file");
+    }
+
+    fn remove_paths_already_in_cache(&self, scanned_paths: Vec<String>) -> Vec<String> {
+        let cached_paths = self.get_all_paths();
+
+        scanned_paths.into_iter()
+                .filter(|x| !cached_paths.contains(x))
+                .collect()
+    }
+    
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     search_dirs_paths: Vec<String>,
     query_img_path: String
 }
-
-const CONFIG_PATH_DEFAULT: &str = "config.json";
-const IMAGE_RESIZE_SIZE: [u32; 2] = [100,100];
 
 /// returns true if path leads to image file,
 /// returns false otherwise
@@ -55,10 +172,9 @@ fn find_image_files(dir_paths: &Vec<String>) -> Vec<String>{
                     println!("{}", direntry.path().display());
                     img_paths.push(direntry.path().to_string_lossy().to_string());
                 }
-                Err(_) => println!("invalid file, skipping")
+                Err(_) => println!("error opening file: {}", path)
             }
         }
-
     }
     println!("--------------------");
 
@@ -74,20 +190,9 @@ fn load_config(filepath: &str) -> Result<Config> {
     serde_json::from_str::<Config>(&data)
 }
 
-// fn get_img_dirs_from_file<P>(filename: P) -> Vec<String>
-// where P: AsRef<Path>, {
-//     let file = File::open(filename).unwrap();
-//     let mut img_dir_paths: Vec<String> = Vec::new();
-//     for line in io::BufReader::new(file).lines() {
-//         img_dir_paths.push(line.unwrap());
-//     }
+fn get_hashes_and_dists(img_paths: Vec<String>, query_img_path: &String) -> (Vec<ImgInfo>, Vec<ImageError>) {
 
-//     img_dir_paths
-// }
-
-fn get_image_hashes_and_dists(img_paths: &Vec<String>, query_img_path: &String) -> (Vec<HashPath>, Vec<ImageError>) {
-
-    let mut hashes: Vec<HashPath> = Vec::new();
+    let mut hashes: Vec<ImgInfo> = Vec::new();
     let mut failed_file_paths: Vec<ImageError> = Vec::new();
 
     /* make hasher object */
@@ -102,13 +207,13 @@ fn get_image_hashes_and_dists(img_paths: &Vec<String>, query_img_path: &String) 
             let query_img_hash = hasher.hash_image(&query_img.resize(IMAGE_RESIZE_SIZE[0], IMAGE_RESIZE_SIZE[1], FilterType::Nearest));
 
             /* get hashes and distances for all search images */
-            for path in tqdm!(img_paths.iter(), desc="calculating image hashes and similarities") {
+            for path in tqdm!(img_paths.iter(), desc="Calculating hashes and dists") {
                 /* load search image file */
                 match image::open(&path) {
                     Ok(img) => {
                         let hash = hasher.hash_image(&img.resize(IMAGE_RESIZE_SIZE[0], IMAGE_RESIZE_SIZE[1], FilterType::Nearest));
                         let dist = hash.dist(&query_img_hash);
-                        hashes.push(HashPath { path: path.clone(), hash: hash , dist: dist});
+                        hashes.push(ImgInfo { path: path.clone(), hash: hash , dist: dist});
                     },
                     Err(err) => {
                         failed_file_paths.push(err);
@@ -122,26 +227,29 @@ fn get_image_hashes_and_dists(img_paths: &Vec<String>, query_img_path: &String) 
     (hashes, failed_file_paths)
 }
 
-#[derive(Debug)]
-struct HashPath{
-    hash: ImageHash,
-    path: String,
-    dist: u32
-}
-
 fn main() {
-    /* parse command line arguments */
-    // let args = ReverseImageSearchArgs::parse();
+
+    /* start a timer */
+    let timer = Instant::now();
 
     /* load config */
     let config = load_config(CONFIG_PATH_DEFAULT).unwrap();
 
-    let timer = Instant::now();
+    /* create new cache handler struct */
+    let mut cache = Cache::new();
 
+    /* get all image file paths in search directories */
     let img_paths = find_image_files(&config.search_dirs_paths);
 
-    let (mut hashinfos, errored_files) = get_image_hashes_and_dists(&img_paths, &config.query_img_path);
+    /* remove paths already in cache */
+    let img_paths = cache.remove_paths_already_in_cache(img_paths);
 
+    /* get hashes for newly-scanned files */
+    let (imginfos, errored_files) = get_hashes_and_dists(img_paths, &config.query_img_path);
+
+    println!("\nhashed {} new files", imginfos.len());
+
+    /* show if any files failed to open */
     if errored_files.len() > 0 {
         println!("the following files failed to open");
         for err in errored_files.iter() {
@@ -149,12 +257,20 @@ fn main() {
         }
     }
 
-    hashinfos.sort_by_key(|x| x.dist);
-    
-    println!("{:#?}", hashinfos);
+    cache.merge(imginfos);
 
-    if hashinfos[0].dist == 0 {
-        println!("EXACT MATCH FOUND");
+    cache.info.sort_by_key(|x| x.dist);
+
+    cache.save();
+    
+    /* print exact matches */
+    for entry in cache.info.iter() {
+        if entry.dist == 0 {
+            println!("MATCH: {:#?}", entry);
+        }
+        else {
+            break;
+        }
     }
 
     println!("took: {:?}", timer.elapsed());
